b7629465e3f893afb4d5301f0dd7da493093aedb
Tarik-Ahmed75 <tarik.ahmed@northsouth.edu>
2023-03-24 22:56:05 +0600

    API fetch done

--- pokemon_app/src/components/Card.jsx
+++ pokemon_app/src/components/Card.jsx
@@ -1,34 +1,55 @@
 // import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+// import CardInfo from "./CardInfo";
+const API = "https://pokeapi.co/api/v2/pokemon/";
 const Card = () => {
   const [myData, setMyData] = useState([]);
+
+  async function getData() {
+    const res = await axios.get(API);
+    getURLData(res.data.results);
+  }
+  async function getURLData(res) {
+    res.map(async (items) => {
+      const info = await axios.get(items.url);
+      setMyData((data) => {
+        data = [...data, info.data];
+        data.sort((a, b) => (a.id > b.id ? 1 : -1));
+        return data;
+      });
+    });
+  }
   useEffect(() => {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/")
-      .then((res) => setMyData(res.data));
+    getData();
   }, []);
-  return myData.map((data) => {
-    <div className="bg-white shadow-md rounded-lg m-6 w-[235px] h-[307] p-3">
-      <img
-        className=" rounded-md h-[215px] w-[215px] "
-        src="./img/Image01.png"
-        alt="product image"
-      />
-      <div className="py-2">
-        <h3 className="font-semibold text-lg">{data.name}</h3>
-        <div className="flex py-2 space-x-2">
-          <h3 className=" px-4 py-.5 text-white rounded bg-green-400  text-sm font-normal ">
-            Grass
-          </h3>
-          <h3 className="px-4 py-.5 text-white rounded bg-purple-400 text-sm font-normal ">
-            Poison
-          </h3>
-        </div>
-      </div>
-    </div>;
-  });
+
+  return (
+    <div className="grid grid-cols-5 absolute top-56  justify-items-center">
+      {myData.map((data) => {
+        return (
+          <div className="bg-white shadow-md rounded-lg m-6 w-[235px] h-[307] p-3">
+            <img
+              className=" rounded-md h-[215px] w-[215px] bg-gray-100"
+              src={data.sprites.other.dream_world.front_default}
+              alt="product image"
+            />
+            <div className="py-2">
+              <h3 className="font-semibold text-lg">{data.name}</h3>
+              <div className="flex py-2 space-x-2">
+                <h3 className=" px-4 py-.5 text-white rounded bg-green-400  text-sm font-normal ">
+                  {data.type}
+                </h3>
+                <h3 className="px-4 py-.5 text-white rounded bg-purple-400 text-sm font-normal ">
+                  Poison
+                </h3>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Card;
--- pokemon_app/src/pages/Home.jsx
+++ pokemon_app/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Card from "../components/Card";
 
 const Home = () => {
   return (
-    <div>
+    <div className="mx-auto">
       <div className="bg-[url('/img/Background.png')] h-[1080px] ">
         <img className="h-[1080px] absolute" src="./img/Texture.png" />
         <div className=" flex justify-center relative">
